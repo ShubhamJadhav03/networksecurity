4adc71edd87e4f9d5206201c818956ba59455b69
Shubham Jadhav <shubhamsj2003@gmail.com>
2025-09-16 01:42:31 +0530

    Model Trainer Component

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.model_trainer import ModelTrainer
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataTransformationConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataTransformationConfig,DataValidationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
- 
+
 
 import sys
 
@@ -34,6 +35,12 @@ if __name__=='__main__':
         logging.info("Initiate the Data Transformation")
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
+
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
+        logging.info("Initiate the Model Trainer")
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        print(model_trainer_artifact)
 
         
     except Exception as e:
